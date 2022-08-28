1caf207851aaaf865edb61c4b4d8a7ae336ed5b8
Vincent Ugwueze <ugwuezev@gmail.com>
2022-08-28 11:16:13 +0100

    api for add, update and delete keywords and universities

--- backend/controllers/keywordController.js
+++ backend/controllers/keywordController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require('express-async-handler')
 const Keyword = require('../models/keywordModel')
 
-// perform query here
+// query to retireve all keywords
 const getKeywords = asyncHandler(async (req, res) => {
 
     const keywords = await Keyword.find();
@@ -9,6 +9,56 @@ const getKeywords = asyncHandler(async (req, res) => {
     res.status(200).json(keywords)
 })
 
+
+// query to add a new keyword
+const addKeyword = asyncHandler(async (req, res) => {
+    if (!req.body.name) {
+        res.status(400)
+        throw new Error("Please add the keyword name")
+    }
+
+    const keyword = await Keyword.create({
+        name: req.body.name,
+    })
+    
+    res.status(200).json(keyword)
+})
+
+
+// query to update keyword details
+const updateKeyword= asyncHandler(async (req, res) => {
+    const keyword = await Keyword.findById(req.params._id)
+
+    if (!keyword) {
+        res.status(400)
+        throw new Error("Keyword not found")
+    }
+
+    const updatedKeyword = await Keyword.findByIdAndUpdate(req.params._id, req.body, {
+        new: true,
+    })
+
+    res.status(200).json(updatedKeyword);
+})
+
+
+// query to remove keyword
+const deleteKeyword = asyncHandler(async (req, res) => {
+    const keyword = await Keyword.findById(req.params._id)
+
+    if (!keyword) {
+        res.status(400)
+        throw new Error("Keyword not found")
+    }
+
+    await keyword.remove()
+
+    res.status(200).json({ id: req.params._id })
+})
+
 module.exports = { 
-    getKeywords
+    getKeywords,
+    addKeyword,
+    updateKeyword,
+    deleteKeyword,
   }
--- backend/controllers/universityController.js
+++ backend/controllers/universityController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const University = require('../models/universityModel');
 
-// perform query here
+// query to retireve all universities
 const getUniversities = asyncHandler(async (req, res) => {
     
     const universities = await University.find();
@@ -9,6 +9,75 @@ const getUniversities = asyncHandler(async (req, res) => {
     res.status(200).json(universities);
 })
 
+
+// query to add a new university
+const addUniversity = asyncHandler(async (req, res) => {
+    if (!req.body.full_name) {
+        res.status(400)
+        throw new Error("Please add the name of the university")
+    }
+
+    if (!req.body.twitter_name) {
+        res.status(400)
+        throw new Error("Please add the name of the university as seen on their twitter page")
+    }
+
+    if (!req.body.twitter_handle) {
+        res.status(400)
+        throw new Error("Please add the university twitter handle without the @ sign")
+    }
+
+    if (!req.body.twitter_avi_link) {
+        res.status(400)
+        throw new Error("Please add the link to the university profile picture")
+    }
+
+    const university = await University.create({
+        full_name: req.body.full_name,
+        twitter_name: req.body.twitter_name,
+        twitter_handle: req.body.twitter_handle,
+        twitter_avi_link: req.body.twitter_avi_link,
+    })
+    
+    res.status(200).json(university)
+})
+
+
+// query to update university details
+const updateUniversity= asyncHandler(async (req, res) => {
+    const university = await University.findById(req.params._id)
+
+    if (!university) {
+        res.status(400)
+        throw new Error("University not found")
+    }
+
+    const updatedUniversity = await University.findByIdAndUpdate(req.params._id, req.body, {
+        new: true,
+    })
+
+    res.status(200).json(updatedUniversity);
+})
+
+
+// query to remove university
+const deleteUniversity = asyncHandler(async (req, res) => {
+    const university = await University.findById(req.params._id)
+
+    if (!university) {
+        res.status(400)
+        throw new Error("University not found")
+    }
+
+    await university.remove()
+
+    res.status(200).json({ id: req.params._id })
+})
+
+
 module.exports = { 
-    getUniversities
+    getUniversities,
+    addUniversity,
+    updateUniversity,
+    deleteUniversity,
   }
